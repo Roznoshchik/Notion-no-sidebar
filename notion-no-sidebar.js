0b18323a59ab987dc99a787e3564f5d64e5c8856
Rostislav Roznoshchik <45914541+Roznoshchik@users.noreply.github.com>
2024-02-15 11:59:29 +0000

    Update notion-no-sidebar.js

--- notion-no-sidebar.js
+++ notion-no-sidebar.js
@@ -11,31 +11,34 @@
 // @run-at document-idle
 // ==/UserScript==
 
-(function() {
+let alreadyCalled = null;
+
+(function () {
     'use strict';
 
     //set global variables
-    var btn;
-    var NScontainer;
-    var Nframe;
-    var Nsidebar;
-    var Ntopbar;
+    let btn, NScontainer, Nframe, Nsidebar, Ntopbar;
+
+
 
     //Because Notion creates the page from JS, need to use this function to wait for everything to load.
-    function a(){
-        waitForKeyElements (".notion-sidebar-container", b);
+    function a() {
+        waitForKeyElements(".notion-sidebar-container", b);
     };
-    function b(){
-        waitForKeyElements (".notion-frame", c);
+    function b() {
+        waitForKeyElements(".notion-frame", c);
     };
-     function c(){
-        waitForKeyElements (".notion-topbar", setVariables);
+    function c() {
+        waitForKeyElements(".notion-topbar", d);
     };
-    waitForKeyElements (".notion-sidebar", a);
+    function d() {
+        waitForKeyElements(".notion-topbar > div", setVariables);
+    };
+    waitForKeyElements(".notion-sidebar", a);
 
 
     //Once everything is loaded, query everything and set initial values.
-    function setVariables(){
+    function setVariables() {
         NScontainer = document.querySelector(".notion-sidebar-container");
         NScontainer.style.visibility = "visible";
 
@@ -45,57 +48,59 @@
         Nsidebar = document.querySelector(".notion-sidebar");
         Nsidebar.style.visibility = "visible";
 
-        Ntopbar = document.querySelector(".notion-topbar").firstChild;
+        Ntopbar = document.querySelector(".notion-topbar");
+
 
         //call the main function.
         isLoaded();
     };
 
     //function that changes the sidebar position and visibility on button click.
-    function vizsidebar()
-    {
+    function vizsidebar() {
 
-        if (NScontainer.style.visibility === "visible")
-        {
+        if (NScontainer.style.visibility === "visible") {
             NScontainer.style.visibility = "hidden";
         }
-        else
-        {
+        else {
             NScontainer.style.visibility = "visible";
         }
 
-        if (Nsidebar.style.visibility === "visible")
-        {
-            Nsidebar.style.visibility = "hidden";
+        if (Nsidebar.style.display === "flex") {
+            Nsidebar.style.display = "none";
         }
-        else
-        {
-            Nsidebar.style.visibility = "visible";
+        else {
+            Nsidebar.style.display = "flex";
+
         }
 
-        if (Nframe.style.position === "relative")
-        {
+        if (Nframe.style.position === "relative") {
             Nframe.style.position = "absolute";
             Nframe.style.width = "100vw";
+            Nframe.style.flexGrow = "1";
         }
-        else
-        {
+        else {
             Nframe.style.position = "relative";
+            Nframe.style.flexGrow = "0";
+
         }
     };
-    
+
     //main function that creates the button and inserts it into the dom
-    function isLoaded(){
+    function isLoaded() {
+        if (alreadyCalled) return;
+        else alreadyCalled = true;
 
         btn = document.createElement("button");
         btn.innerHTML = '<svg viewBox="0 0 14 14" style="width: 14px; height: 14px; display: block; fill: rgba(55, 53, 47, 0.8); flex-shrink: 0; backface-visibility: hidden;" class="doubleChevronLeft"><path d="M7 12.025L8.225 13.25L14 7.125L8.225 1L7 2.225L11.55 7.125L7 12.025ZM0 12.025L1.225 13.25L7 7.125L1.225 1L8.56743e-07 2.225L4.55 7.125L0 12.025Z"></path></svg>';
-
+        btn.classList.add('NOTION-SIDEBAR-HACK');
         btn.style.cssText = "background-color: #f4511e; border: none; color: white; padding: 8px 8px; text-align: center; font-size: 16px; margin: 4px 2px; opacity: 0.6; transition: 0.3s; display: inline-block; text-decoration: none; cursor: pointer;"
 
-        Ntopbar.insertBefore(btn, Ntopbar.firstChild);
+        const child = Ntopbar.firstElementChild;
+
+        child ? child.insertBefore(btn, child.firstChild) : Ntopbar.insertBefore(btn, Ntopbar.firstChild);
 
         // sets the onclick function for the button. 
-        btn.onclick = function(){
+        btn.onclick = function () {
             vizsidebar();
         };
     };
